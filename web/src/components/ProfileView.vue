b51f686bc4ca58e0da14f58daf898a1d08e2b063
gaoxiang <4008353@qq.com>
2020-11-19 15:20:12 +0800

    个人页组件

--- web/src/components/ProfileView.vue
+++ web/src/components/ProfileView.vue
@@ -1,6 +1,18 @@
 <template>
-  <Row>
-    profile view
+  <Row class="component">
+    <h2 class="font-title mb-6">Profile</h2>
+    <Card class="profile-box mb-6">
+      <div style="text-align:center">
+        <Avatar icon="ios-person" size="large" class="mb-6" />
+        <h3 class="nick">Gurudin</h3>
+        <p>Bootstrap is an open source toolkit for developing web with HTML.</p>
+      </div>
+    </Card>
+
+    <Col :span="24" class="footer-bottom">
+      Logout
+      <Icon type="md-log-out" />
+    </Col>
   </Row>
 </template>
 
