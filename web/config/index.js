ffe55530b03c544ac6c1c8cb2b30b9c967bcc2d5
gaoxiang <4008353@qq.com>
2020-11-20 17:17:40 +0800

    build

--- web/config/index.js
+++ web/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/web/dist',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
